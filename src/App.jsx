8d7a7790d69599050995ec718680339fad23a8ec
Runner-Dad84 <vanhoogie@gmail.com>
2025-01-16 22:04:42 -0500

    imageId array with ids only. Map images using image component in fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import ImageComponent  from './Fetch.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const imageIds = ['7970076', '1483681', '789501', '3619698', '2245743', '3584313', '3619698', '4406410', '7170837', '6566978', ]
+  const images = imageIds.map((id) =>
+    <ImageComponent key={id} id={id}
+   />
+  )
 
+//setimageID(ImageIds[Math.floor(Math.random() * ImageIds.length)].id);
 
   return (
     <div>
-      <ImageComponent />
-      <ImageComponent />
-      <ImageComponent />
-    </div>
-
-
-    
+      {images}
+    </div>  
   )
 }
 
