97a669436456ea4fdb92ba612c3929bf3c6f1c2c
Runner-Dad84 <vanhoogie@gmail.com>
2025-01-17 22:04:57 -0500

    radomize function to create random array for img on handler

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,18 @@ import './App.css'
 function App() {
   const [selected, setSelected ] = useState([]);
   
-  const imageIds = ['7970076', '1483681', '789501', '3619698', '2245743', '3584313', '3619698', '4406410', '7170837', '6566978', ]
-  const images = imageIds.map((id) =>
+  const imageIds = ['7970076', '1483681', '789501', '3619698', '2245743', '3584313', '3619698', '4406410', '7170837', '6566978']
+  function randomize (array) {
+    for (let i = array.length -1; i > 0; i--){
+      let j = Math.floor(Math.random() * i + 1);
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+    return array;
+  }
+  const randomArray = randomize(imageIds);
+
+
+  const images = randomArray .map((id) =>
     <ImageComponent 
       key={id} 
       id={id}
