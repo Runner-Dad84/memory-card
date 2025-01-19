d4cd0af044fbd70a4942632a240166181f90484f
Runner-Dad84 <vanhoogie@gmail.com>
2025-01-18 21:46:04 -0500

    Changed random to shuffuled and set value to a new array. Changed the (i + 1) so that the first image is not always the sane

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ import './App.css'
 function App() {
   const [selected, setSelected ] = useState([]);
   
-  const imageIds = ['7970076', '1483681', '789501', '3619698', '2245743', '3584313', '3619698', '4406410', '7170837', '6566978']
+  const imageIds = ['7970076', '1483681', '789501', '3619698', '2245743', '3584313', '7417091', '4406410', '7170837', '6566978']
   function randomize (array) {
+    let shuffled = [...array]
     for (let i = array.length -1; i > 0; i--){
-      let j = Math.floor(Math.random() * i + 1);
-      [array[i], array[j]] = [array[j], array[i]]
+      let j = Math.floor(Math.random() * (i + 1));
+      [shuffled [i], shuffled [j]] = [shuffled [j], shuffled [i]]
     }
-    return array;
+    return shuffled;
   }
-  const randomArray = randomize(imageIds);
+  const shuffledArray = randomize(imageIds);
 
 
-  const images = randomArray .map((id) =>
+  const images = shuffledArray.map((id) =>
     <ImageComponent 
       key={id * Math.random()} 
       id={id}
       setSelected={setSelected}
    />
   )
-
   return (
     <div>
       {images}
